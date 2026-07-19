fb2d2bc5bfa536fff3cec96ade6932931253ba7b
Brenda Brito <bbritob8@gmail.com>
2026-07-19 10:55:33 -0300

    feat: add caixa

---
+++ ControleCaixa.Model/Entities/Caixa.cs
@@ -0,0 +1,28 @@
+namespace ControleCaixa.Model;
+
+public class Caixa
+{
+    public int Id { get; private set; }
+    public string Nome { get; private set; }
+    public decimal SaldoMinimo { get; private set; }
+    public decimal Saldo { get; private set; }
+    public DateTime DataCriacao { get; private set; }
+    public DateTime DataAtualizacao { get; private set; }
+    public bool Lixeira { get; private set; }
+    
+    public List<Movimentacao> Movimentacoes { get; private set; }
+
+    public Caixa(){}
+
+    public Caixa(string nome, decimal saldoMinimo, DateTime dataCriacao)
+    {
+        Nome = nome;
+        SaldoMinimo = saldoMinimo;
+        DataCriacao = dataCriacao;
+    }
+    
+    public void DefinirNome(string nome) => Nome = nome;
+    public void DefinirSaldoMinimo(decimal saldoMinimo) => SaldoMinimo = saldoMinimo;
+    public void EnviarParaLixeira() => Lixeira = true;
+    
+}
