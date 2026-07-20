7abe9828402ff43a56f091ab3cf392a8339bfe73
Brenda Brito <bbritob8@gmail.com>
2026-07-20 14:04:52 -0300

    feat: context

---
+++ ControleCaixa.Data/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using ControleCaixa.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleCaixa.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Caixa> Caixas => Set<Caixa>();
+    public DbSet<Movimentacao> Movimentacoes => Set<Movimentacao>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Ignore<ValidationResult>();
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
