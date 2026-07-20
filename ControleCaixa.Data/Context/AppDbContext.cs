3b7dbc0a6cae2e19f65efa03c6a2d0956b20e9f6
Brenda Brito <bbritob8@gmail.com>
2026-07-20 14:54:38 -0300

    feat: include uow

--- ControleCaixa.Data/Context/AppDbContext.cs
+++ ControleCaixa.Data/Context/AppDbContext.cs
@@ -1,10 +1,10 @@
-using System.ComponentModel.DataAnnotations;
+using ControleCaixa.Data.Interfaces;
 using ControleCaixa.Model.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ControleCaixa.Data.Context;
 
-public class AppDbContext : DbContext
+public class AppDbContext : DbContext, IUnitOfWork
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
@@ -13,11 +13,11 @@ public class AppDbContext : DbContext
 
     public DbSet<Caixa> Caixas => Set<Caixa>();
     public DbSet<Movimentacao> Movimentacoes => Set<Movimentacao>();
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Ignore<ValidationResult>();
-
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
     }
 }
---
+++ ControleCaixa.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+namespace ControleCaixa.Data.Interfaces;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
