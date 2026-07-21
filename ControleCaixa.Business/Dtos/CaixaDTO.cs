7d512d35a49074ab48a7ffa9f4556c410cfdf138
Brenda Brito <bbritob8@gmail.com>
2026-07-21 16:37:38 -0300

    fiz: retirando dtos especificos para edição

--- ControleCaixa.Business/Dtos/CaixaDTO.cs
+++ ControleCaixa.Business/Dtos/CaixaDTO.cs
@@ -5,10 +5,3 @@ public class CaixaDTO
     public string Nome { get; set; }
     public decimal SaldoMinimo { get; set; }
 }
-
-public class CaixaEditarDTO
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public decimal SaldoMinimo { get; set; }
-}
--- ControleCaixa.Business/Dtos/MovimentacaoDTO.cs
+++ ControleCaixa.Business/Dtos/MovimentacaoDTO.cs
@@ -4,28 +4,13 @@ namespace ControleCaixa.Business.Dtos;
 
 public class MovimentacaoDTO
 {
-    public string Descricao { get; private set; }
+    public string Descricao { get; set; }
 
-    public TipoMovimentacao Tipo { get; private set; }
+    public TipoMovimentacao Tipo { get; set; }
 
-    public Categoria Categoria { get; private set; }
+    public Categoria Categoria { get; set; }
 
-    public decimal Valor { get; private set; }
+    public decimal Valor { get; set; }
     
-    public int CaixaId { get; private set; }
-}
-
-public class MovimentacaoEditarDTO
-{
-    public int Id { get; private set; }
-
-    public string Descricao { get; private set; }
-
-    public TipoMovimentacao Tipo { get; private set; }
-
-    public Categoria Categoria { get; private set; }
-
-    public decimal Valor { get; private set; }
-    
-    public int CaixaId { get; private set; }
+    public int CaixaId { get; set; }
 }
