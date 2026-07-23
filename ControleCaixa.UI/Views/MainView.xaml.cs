56d75baa11d76e8db1277650ae46d67edb3dd8e3
Brenda Brito <bbritob8@gmail.com>
2026-07-23 18:16:15 -0300

    feat: tela inicial

---
+++ ControleCaixa.UI/Views/MainView.xaml.cs
@@ -0,0 +1,26 @@
+using System.Windows;
+using ControleCaixa.UI.ViewModels;
+
+namespace ControleCaixa.UI.Views;
+
+public partial class MainView : Window
+{
+    private readonly MainViewModel _viewModel;
+
+    public MainView(MainViewModel viewModel)
+    {
+        InitializeComponent();
+
+        _viewModel = viewModel;
+        DataContext = viewModel;
+
+        Loaded += MainView_Loaded;
+    }
+
+    private async void MainView_Loaded(
+        object sender,
+        RoutedEventArgs e)
+    {
+        await _viewModel.CarregarCaixasCommand.ExecuteAsync(null);
+    }
+}
